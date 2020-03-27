70a78c337978848a323b1820510e7bd6699869f0
Alex Cone <alexcone@live.com>
2020-03-27 17:47:22 -0500

    Updates

---
+++ precache-manifest.bc6076db1ef206be54c5c8b495f1bd43.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d2c2ae630e49c6889b052bfae09c74",
+    "url": "/corey-soccer-site/index.html"
+  },
+  {
+    "revision": "6a477d87f7adb5455c9f",
+    "url": "/corey-soccer-site/static/css/main.631b7064.chunk.css"
+  },
+  {
+    "revision": "39db29ca3984a3b13bcc",
+    "url": "/corey-soccer-site/static/js/2.b7a0605a.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/corey-soccer-site/static/js/2.b7a0605a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a477d87f7adb5455c9f",
+    "url": "/corey-soccer-site/static/js/main.8f7d016c.chunk.js"
+  },
+  {
+    "revision": "25dbc50f07d76bf188e3",
+    "url": "/corey-soccer-site/static/js/runtime-main.70ba0447.js"
+  },
+  {
+    "revision": "f706760fe035543a82ebcf601493fe80",
+    "url": "/corey-soccer-site/static/media/pic1.f706760f.jpg"
+  },
+  {
+    "revision": "2bf9c0104d92e5dbbf8eb03f159b6926",
+    "url": "/corey-soccer-site/static/media/pic2.2bf9c010.jpg"
+  },
+  {
+    "revision": "a70f2ef08bc4da8b28820bb73e7dbedd",
+    "url": "/corey-soccer-site/static/media/pic3.a70f2ef0.jpg"
+  }
+]);

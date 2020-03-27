ed03a7477bfab88b65c2383d12133f74881e67ac
Alex Cone <alexcone@live.com>
2020-03-27 18:20:31 -0500

    add profile picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Container, Grid, Image, Menu } from 'semantic-ui-react';
 import './App.css';
 import Carousel from './Carousel';
@@ -9,6 +9,7 @@ import Stats from './Stat';
 const picture1 = require('./resources/pic1.jpg');
 const picture2 = require('./resources/pic2.jpg');
 const picture3 = require('./resources/pic3.jpg');
+const profile = require('./resources/profile.jpg');
 
 const currentAge = moment.duration(moment.now() - 1137232800000).years();
 
@@ -43,17 +44,18 @@ if (document.location.hash) {
 }
 
 function App() {
+  const [id, setId] = useState(document.location.hash.replace('#', '').replace('/', ''));
   return (
     <Grid>
       <Grid.Row>
         <Container style={{ width: '50%' }}>
           <Menu widths={6} stackable fluid>
-            <Menu.Item href='#profile' key="profile" active name="Profile" />
-            <Menu.Item href='#highlights' key="highlights" active name="Highlights" />
-            <Menu.Item href='#breakaways' key="breakaways" active name="Breakaways" />
-            <Menu.Item href='#catching' key="catching" active name="Catching" />
-            <Menu.Item href='#goalkicks' key="goalkicks" active name="Goal Kicks" />
-            <Menu.Item href='#punts' key="punts" active name="Punts" />
+            <Menu.Item href='#profile' key="profile" active={id === 'profile'} name="Profile" onClick={() => setId('profile')} />
+            <Menu.Item href='#highlights' key="highlights" active={id === 'highlights'} name="Highlights" onClick={() => setId('highlights')} />
+            <Menu.Item href='#breakaways' key="breakaways" active={id === 'breakaways'} name="Breakaways" onClick={() => setId('breakaways')} />
+            <Menu.Item href='#catching' key="catching" active={id === 'catching'} name="Catching" onClick={() => setId('catching')} />
+            <Menu.Item href='#goalkicks' key="goalkicks" active={id === 'goalkicks'} name="Goal Kicks" onClick={() => setId('goalkicks')} />
+            <Menu.Item href='#punts' key="punts" active={id === 'punts'} name="Punts" onClick={() => setId('punts')} />
           </Menu>
           <Carousel
             elements={elements}
@@ -65,7 +67,7 @@ function App() {
           <Card.Group id='profile' fluid itemsPerRow={2}>
             <Card
               style={{ width: '50%' }}
-              image='https://react.semantic-ui.com/images/avatar/large/matthew.png'
+              image={profile}
               header='Corey Chachere'
               meta={`${currentAge} years old (Born 01/14/2006)`}
               description={Stats}
